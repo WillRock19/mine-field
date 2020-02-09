081d78289813623d89f879dfb81de6f20b17dcd1
William P. Pinto <william.pinto@lambda3.com.br>
2020-02-09 12:23:37 -0300

    Refactored MineField.js to make it a little more readable

--- mines/src/components/MineField/MineField.js
+++ mines/src/components/MineField/MineField.js
@@ -4,16 +4,29 @@ import {View} from 'react-native';
 import styles from './styles';
 import Field from '../Field';
 
-export default props => {
-  const rows = props.board.map((row, rowIndex) => {
-    const columns = row.map((fieldInformation, fieldIndex) => {
-      return <Field {...fieldInformation} key={fieldIndex} />;
-    });
-    return (
-      <View key={rowIndex} style={styles.columnStyle}>
-        {columns}
-      </View>
-    );
+const renderColumns = (row, rowIndex) => {
+  const columns = row.map((fieldInformation, fieldIndex) => {
+    return <Field {...fieldInformation} key={fieldIndex} />;
   });
-  return <View style={styles.container}>{rows}</View>;
+
+  return (
+    <View key={rowIndex} style={styles.columnStyle}>
+      {columns}
+    </View>
+  );
+};
+
+const renderRowsWithColumns = board => {
+  return board.map((row, rowIndex) => {
+    return renderColumns(row, rowIndex);
+  });
+};
+
+const renderField = board => {
+  const rowsWithColumns = renderRowsWithColumns(board);
+  return <View style={styles.container}>{rowsWithColumns}</View>;
+};
+
+export default props => {
+  return renderField(props.board);
 };
