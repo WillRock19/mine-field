3202f00e6101cb82f81ca3c7cd06d17cb171f12f
William P. Pinto <william.pinto@lambda3.com.br>
2020-02-09 22:08:58 -0300

    Adding functions for game Logic

--- mines/src/services/boardManagerService.js
+++ mines/src/services/boardManagerService.js
@@ -11,6 +11,7 @@ const createBoard = (rows, columns) => {
             hasFlag: false,
             isMined: false,
             opened: false,
+            exploded: false,
             nearMines: 0,
           };
         });
@@ -40,4 +41,87 @@ const createMinedBoard = (rows, column, minesAmount) => {
   return board;
 };
 
-export {createMinedBoard};
+const cloneBoard = board => {
+  return board.map(rows => {
+    return rows.map(field => {
+      return {...field};
+    });
+  });
+};
+
+const getNeighbors = (board, rowToLook, columnToLook) => {
+  const validNeighbors = [];
+  const allNeighborsRows = [rowToLook - 1, rowToLook, rowToLook + 1];
+  const allNeighborsColumns = [
+    columnToLook - 1,
+    columnToLook,
+    columnToLook + 1,
+  ];
+
+  allNeighborsRows.forEach(row => {
+    allNeighborsColumns.forEach(column => {
+      const isNotInitialCellToLook =
+        row !== rowToLook && column !== columnToLook;
+
+      const rowIsValid = row >= 0 && row < board.length;
+      const columnIsValid = column >= 0 && column < board[0].length;
+
+      if (isNotInitialCellToLook && rowIsValid && columnIsValid)
+        validNeighbors.push(board[row][column]);
+    });
+  });
+  return validNeighbors;
+};
+
+const safeNeighborhood = (board, rowToLook, columnToLook) => {
+  const isSafeFunction = (result, neighbor) => result && !neighbor.isMined;
+  const neighbors = getNeighbors(board, rowToLook, columnToLook);
+
+  return neighbors.reduce(isSafeFunction, true);
+};
+
+const openField = (board, rowToLook, columnToLook) => {
+  const selectedField = board[rowToLook][columnToLook];
+
+  if (!selectedField.opened) {
+    selectedField.opened = true;
+
+    if (selectedField.isMined) {
+      selectedField.exploded = true;
+    } else if (safeNeighborhood(board, rowToLook, columnToLook)) {
+      getNeighbors(board, rowToLook, columnToLook).forEach(neighbor =>
+        openField(board, neighbor.row, neighbor.column),
+      );
+    } else {
+      const neighbors = getNeighbors(board, rowToLook, columnToLook);
+      selectedField.nearMines = neighbors.filter(
+        neightbor => neightbor.mined,
+      ).length;
+    }
+  }
+};
+
+const fields = board => [].concat(...board);
+
+const hadExplosion = board =>
+  fields(board).filter(field => field.exploded).length > 0;
+
+const pendding = field => {
+  (field.isMined && !field.hasFlag) || (!field.isMined && !field.opened);
+};
+
+const playerWonGame = board => fields(board).filter(pendding).length === 0;
+
+const showAllMines = board =>
+  fields(board)
+    .filter(field => field.isMined)
+    .forEach(field => field.opened === true);
+
+export {
+  createMinedBoard,
+  cloneBoard,
+  openField,
+  hadExplosion,
+  playerWonGame,
+  showAllMines,
+};
